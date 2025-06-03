e8cca1f9a6a114a8fbcb08a18f52bd1abb10c42a
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2025-06-03 13:42:19 +0200

    Update update.ps1

--- docker/update.ps1
+++ docker/update.ps1
@@ -1,9 +1,26 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Devsh-Graphics-Programming/Nabla/master/compose.yml" -OutFile "compose.yml"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Devsh-Graphics-Programming/Nabla/master/compose.yml" `
+    -OutFile "compose.yml" `
+    -Headers @{
+        "Cache-Control" = "no-cache"
+        "Pragma"        = "no-cache"
+    }
 
-docker compose -f compose.yml down
+try {
+    docker compose -f compose.yml down
+} catch {
+    Write-Host "Warning: docker compose down failed, continuing..."
+}
 
-docker network ls --filter "type=custom" -q | ForEach-Object {
-    docker network rm $_
+try {
+    docker network ls --filter "type=custom" -q | ForEach-Object {
+        try {
+            docker network rm $_
+        } catch {
+            Write-Host "Warning: Failed to remove network ID $_"
+        }
+    }
+} catch {
+    Write-Host "Warning: network list/remove failed, continuing..."
 }
 
 docker compose -f compose.yml up -d --pull always --force-recreate
