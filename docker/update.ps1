fd452eb75a71f43586e6a11eef639b7ed5ab62af
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2025-06-03 13:13:00 +0200

    Update and rename update.bat to update.ps1

---
+++ docker/update.ps1
@@ -0,0 +1,9 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Devsh-Graphics-Programming/Nabla/master/compose.yml" -OutFile "compose.yml"
+
+docker network ls --filter "type=custom" -q | ForEach-Object {
+    docker network rm $_
+}
+
+docker compose -f compose.yml up -d --pull always --force-recreate
+
+docker image prune -f
